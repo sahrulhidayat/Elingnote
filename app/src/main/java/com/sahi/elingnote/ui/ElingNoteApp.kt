7f8debb5d056a380779bb7138d0b19749841bed9
sahrulhidayat <83418069+sahrulhidayat@users.noreply.github.com>
2023-07-30 01:21:26 +0700

    Update floating action button
    The icon on the fab now adjusts to the active page

--- app/src/main/java/com/sahi/elingnote/ui/ElingNoteApp.kt
+++ app/src/main/java/com/sahi/elingnote/ui/ElingNoteApp.kt
@@ -1,26 +1,14 @@
 package com.sahi.elingnote.ui
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Checklist
-import androidx.compose.material.icons.filled.Notes
+import androidx.compose.material.icons.filled.AddTask
+import androidx.compose.material.icons.filled.NoteAdd
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -33,7 +21,6 @@ import com.sahi.elingnote.ui.components.ElingNoteNavigationBar
 import com.sahi.elingnote.ui.components.ElingNoteNavigationBarItem
 import com.sahi.elingnote.ui.note_feature.edit_note.navigateToEditNote
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ElingNoteApp(
     appState: ElingNoteAppState = rememberElingNoteAppState()
@@ -41,22 +28,38 @@ fun ElingNoteApp(
 
     val snackbarHostState = remember { SnackbarHostState() }
 
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-    val bottomSheetState = rememberModalBottomSheetState()
-
     val destination = appState.currentTopLevelDestination
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
         floatingActionButton = {
-            if (destination != null) {
+            if (destination != null && destination != TopLevelDestination.TRASH) {
                 FloatingActionButton(
-                    onClick = { openBottomSheet = !openBottomSheet },
+                    onClick = {
+                        when (destination) {
+                            TopLevelDestination.NOTE -> appState.navController.navigateToEditNote()
+                            TopLevelDestination.CHECKLIST -> appState.navController.navigateToEditChecklist()
+                            else -> {}
+                        }
+                    },
                     containerColor = MaterialTheme.colorScheme.primary,
                     elevation = FloatingActionButtonDefaults.elevation(0.dp),
-                ) {
-                    Icon(Icons.Default.Add, "Add button")
-                }
+                    content = {
+                        when (destination) {
+                            TopLevelDestination.NOTE -> Icon(
+                                imageVector = Icons.Default.NoteAdd,
+                                contentDescription = "New note"
+                            )
+
+                            TopLevelDestination.CHECKLIST -> Icon(
+                                imageVector = Icons.Default.AddTask,
+                                contentDescription = "New checklist"
+                            )
+
+                            else -> {}
+                        }
+                    }
+                )
             }
         },
         bottomBar = {
@@ -79,51 +82,6 @@ fun ElingNoteApp(
                 .padding(padding)
         ) {
             ElingNoteNavHost(navController = appState.navController)
-        }
-
-        if (openBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = { openBottomSheet = false },
-                sheetState = bottomSheetState
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start
-                ) {
-                    LazyColumn {
-                        items(1) {
-                            ListItem(
-                                modifier = Modifier
-                                    .clickable {
-                                        appState.navController.navigateToEditNote()
-                                        openBottomSheet = !openBottomSheet
-                                    },
-                                headlineContent = { Text(text = "New note") },
-                                leadingContent = {
-                                    Icon(Icons.Filled.Notes, contentDescription = "New note")
-                                },
-                            )
-                            ListItem(
-                                modifier = Modifier
-                                    .clickable {
-                                        appState.navController.navigateToEditChecklist()
-                                        openBottomSheet = !openBottomSheet
-                                    },
-                                headlineContent = { Text(text = "New checklist") },
-                                leadingContent = {
-                                    Icon(
-                                        Icons.Filled.Checklist,
-                                        contentDescription = "New checklist"
-                                    )
-                                }
-                            )
-                        }
-                        item {
-                            Spacer(modifier = Modifier.height(32.dp))
-                        }
-                    }
-                }
-            }
         }
     }
 }
